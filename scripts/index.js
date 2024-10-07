43f5f51d57a7f5fe33497d345e4daa8ad4838534
QuantumChemist <christina.ertural@ac.rwth-aachen.de>
2024-10-07 23:41:58 +0200

    edit js

--- scripts/index.js
+++ scripts/index.js
@@ -1,99 +1,100 @@
-// Select the body or main container where you'll append the content
-const body = document.body;
+// Clear the body before adding content to prevent duplication
+document.body.innerHTML = '';
 
-// Create an image element and append it
-const imgRight = document.createElement('img');
-imgRight.src = "https://github.com/QuantumChemist/QuantumChemist.github.io/blob/main/utils/pic3.png?raw=true";
-imgRight.style.width = "20%";
-imgRight.style.height = "10%";
-imgRight.style.hspace = "20";
-imgRight.style.float = "right";
-body.appendChild(imgRight);
+// Create a container for all content
+const container = document.createElement('div');
+container.classList.add('flex-container');
+document.body.appendChild(container);
 
-// Create a paragraph element for intro text
-const introText = document.createElement('p');
-introText.innerHTML = `
-  Hi, I'm <a href="https://g.co/kgs/Bi3Bdb">Chris</a>, scientist in the theoretical and solid-state chemistry community, currently PostDoc in materials chemistry and materials informatics at 
-  <a href="https://www.bam.de/">BAM</a> 
-  (<a href="https://www.bam.de/Content/EN/Standard-Articles/About-us/Jobs-and-Careers/Young-Science/junior-research-group-george-computational-materials-design.html">George group</a>) 
-  and former <a href="http://www.cohp.de/">LOBSTER</a> developer. Here you can find more information about my research.
+// Create a header
+const header = document.createElement('h1');
+header.innerText = "Welcome to my GitHub Pages site!";
+container.appendChild(header);
+
+// Add subheading or subtitle
+const subHeader = document.createElement('p');
+subHeader.innerHTML = 'Visit the <a href="https://g.co/kgs/Bi3Bdb">2020 – 2024 website!</a>';
+container.appendChild(subHeader);
+
+// Create a paragraph for introduction
+const introParagraph = document.createElement('p');
+introParagraph.innerHTML = `
+    Hi, I'm <a href="https://g.co/kgs/Bi3Bdb">Chris</a>, scientist in the theoretical and solid-state chemistry community, 
+    currently PostDoc in materials chemistry and materials informatics at <a href="https://www.bam.de/">BAM</a> 
+    (<a href="https://www.bam.de/Content/EN/Standard-Articles/About-us/Jobs-and-Careers/Young-Science/junior-research-group-george-computational-materials-design.html">George group</a>)
+    and former <a href="http://www.cohp.de/">LOBSTER</a> developer. Here you can find more information about my research.
 `;
-body.appendChild(introText);
+container.appendChild(introParagraph);
 
-// Create an image link for LobsterPyGUI
-const lobsterImage = document.createElement('img');
-lobsterImage.src = "https://quantumchemist.github.io/utils/QClogohighres.png";
-lobsterImage.style.width = "15%";
-lobsterImage.style.float = "left";
+// Add a circular image (research-related)
+const researchImg = document.createElement('img');
+researchImg.src = "https://github.com/QuantumChemist/QuantumChemist.github.io/blob/main/utils/pic3.png?raw=true";
+researchImg.classList.add('left-align'); // Floating the image to the left
+container.appendChild(researchImg);
 
-const lobsterLink = document.createElement('a');
-lobsterLink.href = "https://doi.org/10.1021/acs.chemmater.1c03349";
-lobsterLink.appendChild(lobsterImage);
-body.appendChild(lobsterLink);
-
-// Add additional research-related content
-const researchText = document.createElement('p');
-researchText.innerHTML = `
-  My research interests involve different areas of theoretical, quantum and computational chemistry as well as material informatics...
-  [thermoelectrical materials](https://doi.org/10.1002/jcc.25822), [cathode and anode materials](https://doi.org/10.1021/acs.chemmater.1c03349) for battery technology or 
-  [phase-change materials](https://doi.org/10.1002/anie.202115778).
+// Add a research description
+const researchDescription = document.createElement('p');
+researchDescription.innerHTML = `
+    My research interests involve different areas of theoretical, quantum, and computational chemistry as well as material informatics...
+    [thermoelectrical materials](https://doi.org/10.1002/jcc.25822), [cathode and anode materials](https://doi.org/10.1021/acs.chemmater.1c03349) for battery technology, 
+    or [phase-change materials](https://doi.org/10.1002/anie.202115778).
 `;
-body.appendChild(researchText);
+container.appendChild(researchDescription);
 
-// Add a custom message in Japanese
+// Add a custom Japanese message
 const japaneseMessage = document.createElement('p');
 japaneseMessage.innerText = "いつも和独のタンデムパートナーを探しています。(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠)";
-body.appendChild(japaneseMessage);
+container.appendChild(japaneseMessage);
 
-// Add a section for LobsterPyGUI
-const lobsterSection = document.createElement('h2');
-lobsterSection.innerText = "LobsterPyGUI";
-body.appendChild(lobsterSection);
+// Section: LobsterPyGUI
+const lobsterSectionHeader = document.createElement('h2');
+lobsterSectionHeader.innerText = "LobsterPyGUI";
+container.appendChild(lobsterSectionHeader);
 
-const videoLobsterPyGUI = document.createElement('video');
-videoLobsterPyGUI.src = "https://quantumchemist.github.io/utils/lpglogo.mp4";
-videoLobsterPyGUI.autoplay = true;
-videoLobsterPyGUI.loop = true;
-videoLobsterPyGUI.muted = true;
-videoLobsterPyGUI.style.width = "30%";
-videoLobsterPyGUI.style.float = "left";
+// Add LobsterPyGUI video
+const lobsterVideo = document.createElement('video');
+lobsterVideo.src = "https://quantumchemist.github.io/utils/lpglogo.mp4";
+lobsterVideo.autoplay = true;
+lobsterVideo.loop = true;
+lobsterVideo.muted = true;
+lobsterVideo.classList.add('left-align'); // Floating the video to the left
+container.appendChild(lobsterVideo);
 
-const lobsterLink2 = document.createElement('a');
-lobsterLink2.href = "https://github.com/QuantumChemist/LobsterPyGUI";
-lobsterLink2.appendChild(videoLobsterPyGUI);
-body.appendChild(lobsterLink2);
+// Add Discord widget
+const discordWidget = document.createElement('iframe');
+discordWidget.src = "https://discord.com/widget?id=1277030477303382026&theme=dark";
+discordWidget.width = "350";
+discordWidget.height = "150";
+discordWidget.allowtransparency = true;
+discordWidget.frameBorder = "0";
+discordWidget.sandbox = "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts";
+container.appendChild(discordWidget);
 
-// Add a chatbot and Discord section
-const discordIframe = document.createElement('iframe');
-discordIframe.src = "https://discord.com/widget?id=1277030477303382026&theme=dark";
-discordIframe.width = "350";
-discordIframe.height = "150";
-discordIframe.allowtransparency = true;
-discordIframe.frameborder = "0";
-discordIframe.sandbox = "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts";
-body.appendChild(discordIframe);
+// Contact section
+const contactHeader = document.createElement('h3');
+contactHeader.innerText = "How you can contact me";
+container.appendChild(contactHeader);
 
-// Add contact information
-const contactSection = document.createElement('h3');
-contactSection.innerText = "How you can contact me";
-body.appendChild(contactSection);
-
+// Create contact list
 const contactList = document.createElement('dl');
 contactList.innerHTML = `
-  <dt>Email &#9993;</dt>
-  <dd><a href="mailto:christina.ertural@bam.de">christina.ertural&commat;bam.de</a></dd>
-  <dt>GitHub</dt>
-  <dd><a href="https://github.com/QuantumChemist">QuantumChemist</a></dd>
-  <dt>LinkedIn</dt>
-  <dd><a href="https://www.linkedin.com/in/christina-ertural/">Dr. Christina Ertural</a></dd>
-  <dt>ResearchGate</dt>
-  <dd><a href="https://www.researchgate.net/profile/Christina-Ertural/">Dr. Christina Ertural</a></dd>
+    <dt>Email &#9993;</dt>
+    <dd><a href="mailto:christina.ertural@bam.de">christina.ertural&commat;bam.de</a></dd>
+    <dt>GitHub</dt>
+    <dd><a href="https://github.com/QuantumChemist">QuantumChemist</a></dd>
+    <dt>LinkedIn</dt>
+    <dd><a href="https://www.linkedin.com/in/christina-ertural/">Dr. Christina Ertural</a></dd>
+    <dt>ResearchGate</dt>
+    <dd><a href="https://www.researchgate.net/profile/Christina-Ertural/">Dr. Christina Ertural</a></dd>
 `;
-body.appendChild(contactList);
+container.appendChild(contactList);
 
-// Add a footer message with copyright information
+// Footer
 const footer = document.createElement('footer');
-footer.innerHTML = `
-  <p>&copy; 2020 -- 2024 Christina Ertural</p>
-`;
-body.appendChild(footer);
+footer.innerHTML = `&copy; 2020 -- 2024 Christina Ertural`;
+container.appendChild(footer);
+
+// Clear any floats to prevent overlap
+const clearDiv = document.createElement('div');
+clearDiv.style.clear = 'both';
+container.appendChild(clearDiv);
